a0be2cbda08ae93e4a18df85f52b4892ea036b04
Scott Martin <scott+school@scottmartin.systems>
2018-03-23 22:46:44 +0000

    ESLint fixes

--- src/services/db.js
+++ src/services/db.js
@@ -7,27 +7,38 @@ const db = pg({
 	port:     process.env.THINGBOARD_DB_PORT,
 	database: process.env.THINGBOARD_DB_NAME,
 	user:     process.env.THINGBOARD_DB_USER,
-	password: process.env.THINGBOARD_DB_PASS
+	password: process.env.THINGBOARD_DB_PASS,
 });
+
+function getSavedSites () {
+	// gary_oldman_everything.jpg
+	db.all('SELECT url, title, description, saved FROM site')
+		.then( savedSites => {
+			return savedSites;
+		})
+		.catch( error => {
+			// error;
+		});
+}
 
 function getThumbnail (url) {
 	db.one('SELECT thumbnail FROM site WHERE url=$1', url)
-	.then( imageData => {
-		return imageData;
-	})
-	.catch( error => {
-		// error;
-	});
+		.then( imageData => {
+			return imageData;
+		})
+		.catch( error => {
+			// error;
+		});
 }
 
 function saveThumbnail (imageData, url) {
 	db.none('INSERT INTO site (thumbnail) VALUES ($1) WHERE url=$2', imageData, url)
-	.then(() => {
-		// success;
-	})
-	.catch(error => {
-		// error;
-	});
+		.then(() => {
+			// success;
+		})
+		.catch(error => {
+			// error;
+		});
 }
 
 module.exports.getThumbnail = getThumbnail;
