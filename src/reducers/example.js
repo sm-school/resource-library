a0aea6156f8ca07f63546e8c74f21dd10b8c9fa9
Scott Martin <scott+school@scottmartin.systems>
2018-03-23 14:30:01 +0000

    Whitespace

--- src/reducers/example.js
+++ src/reducers/example.js
@@ -1,10 +1,10 @@
 const example = (state = '', action) => {
-  switch (action.type) {
-    case 'UPDATE_EXAMPLE':
-        return action.example;
-    default:
-      return state;
-  }
+	switch (action.type) {
+		case 'UPDATE_EXAMPLE':
+			return action.example;
+		default:
+			return state;
+	}
 };
 
 export default example;
